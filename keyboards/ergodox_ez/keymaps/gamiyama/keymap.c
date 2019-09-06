3fd4266d21561f9ee3696c86e6ae21bf0bfa1777
gamiyama3110 <gamiyamatatoo3198@gmail.com>
2019-09-07 08:00:04 +0900

    change keymap

--- keyboards/ergodox_ez/keymaps/gamiyama/keymap.c
+++ keyboards/ergodox_ez/keymaps/gamiyama/keymap.c
@@ -13,7 +13,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [L0] = KEYMAP(
+  [L0] = LAYOUT_ergodox(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(KC_LSFT,KC_SPC), LT(L2,JP_MEISU), XXXXXXX,
 
     XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-    XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+    XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,
     XXXXXXX, JP_MKANA, KC_ENT
   ),
-  [L1] = KEYMAP(
+  [L1] = LAYOUT_ergodox(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,
     _______, KC_F11,  KC_F12,  _______, _______, _______, _______,
     _______, JP_DOT,  JP_SLSH, JP_ASTR, JP_MINS, JP_PLUS,
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,
 
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-    _______, _______, _______, _______, _______, _______, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, KC_DEL,
              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, JP_SCLN, JP_COLN, JP_LBRC, JP_RBRC, JP_BSLS, _______,
                       _______, _______, _______, _______, _______,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,
     _______, _______, _______
   ),
-  [L2] = KEYMAP(
+  [L2] = LAYOUT_ergodox(
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,
     _______, _______, _______
   ),
-  [L3] = KEYMAP(
+  [L3] = LAYOUT_ergodox(
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(L0)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -130,10 +130,10 @@ void matrix_scan_user(void) {
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
-        case SYMB:
+        case L0:
             ergodox_right_led_1_on();
             break;
-        case META:
+        case L1:
             ergodox_right_led_3_on();
             break;
         default:
