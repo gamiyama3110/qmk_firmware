20586a9e982bf1841f667ec5da7c0a756291abed
t_yamagami <t_yamagami@fancs.com>
2019-09-13 15:05:44 +0900

    update

--- keyboards/ergodox_ez/keymaps/gamiyama/keymap.c
+++ keyboards/ergodox_ez/keymaps/gamiyama/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
     XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
-    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
                       MO(L1),  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX,
     XXXXXXX,
